59a0cf70b14f63e71972bbf501f643a3cc7fdd8a
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-11-17 19:08:05 -0500

    base setup for portfolio, finaly got useEffect to work and applied it to all pages

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+
+
+const Portfolio = () => { 
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    });
+
+    useEffect(() => {
+       
+    }, []);
+
+    return (
+        <>
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={"Portfolio".split("")}
+                        idx={15}
+                    />
+                </h1>
+                
+            </div>
+            
+        </>
+    );
+}
+export default Portfolio;
