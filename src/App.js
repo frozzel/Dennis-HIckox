eebdd448eb6a7b649a1d04f7cff2bbfa8b9684aa
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-12-05 20:03:33 -0500

    last test failed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <>
       
     <Routes >
-    <Route path="dennis-hickox/" element={<Layout />}>
+    <Route path="/" element={<Layout />}>
        <Route index element={<Home />} />
        <Route path="/about" element={<About />} />
        <Route path="/contact" element={<Contact />} />
